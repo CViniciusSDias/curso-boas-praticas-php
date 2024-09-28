edcadb3490b0eff48695d32a655b198319a9be11
Vinicius Dias <carlosv775@gmail.com>
2024-09-28 19:45:17 -0300

    Video 1.3

--- projeto-inicial.php
+++ projeto-inicial.php
@@ -16,80 +16,11 @@ try {
         if ($opcaoEscolhida == 1) {
             listarAbrigos();
         } else if ($opcaoEscolhida == 2) {
-            echo "Digite o nome do abrigo:" . PHP_EOL;
-            $nome = trim(fgets(STDIN));
-            echo "Digite o telefone do abrigo:" . PHP_EOL;
-            $telefone = trim(fgets(STDIN));
-            echo "Digite o email do abrigo:" . PHP_EOL;
-            $email = trim(fgets(STDIN));
-
-            $abrigo = compact('nome', 'telefone', 'email');
-
-            $curl = curl_init('https://66f610a1436827ced975d41f.mockapi.io/abrigos');
-            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($curl, CURLOPT_POST, true);
-            curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($abrigo));
-            curl_setopt($curl, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
-
-            $responseBody = curl_exec($curl);
-            $statusCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-            if ($statusCode < 400) {
-                echo "Abrigo cadastrado com sucesso!" . PHP_EOL;
-            } else {
-                echo "Erro ao cadastrar o abrigo:" . PHP_EOL;
-                echo $responseBody . PHP_EOL;
-            }
+            cadastrarAbrigo();
         } else if ($opcaoEscolhida == 3) {
-            echo "Digite o id do abrigo:" . PHP_EOL;
-            $idOuNome = trim(fgets(STDIN));
-            $response = @file_get_contents("https://66f610a1436827ced975d41f.mockapi.io/abrigos/$idOuNome/pets");
-            if (!$response) {
-                echo "Id não cadastrado!" . PHP_EOL;
-                continue;
-            }
-            $jsonArray = json_decode($response, true);
-            echo "Pets cadastrados:" . PHP_EOL;
-            foreach ($jsonArray as $pet) {
-                extract($pet);
-                echo "$id - $tipo - $nome - $raca - $idade ano(s)" . PHP_EOL;
-            }
+            listarPetsDeAbrigo();
         } else if ($opcaoEscolhida == 4) {
-            echo "Digite o id do abrigo:" . PHP_EOL;
-            $idOuNome = trim(fgets(STDIN));
-
-            echo "Digite o nome do arquivo CSV:" . PHP_EOL;
-            $nomeArquivo = trim(fgets(STDIN));
-
-            $arquivo = fopen($nomeArquivo, 'r');
-            while ($campos = fgetcsv($arquivo)) {
-                $pet = [
-                    'tipo' => $campos[0],
-                    'nome' => $campos[1],
-                    'raca' => $campos[2],
-                    'idade' => $campos[3],
-                    'cor' => $campos[4],
-                    'peso' => $campos[5],
-                ];
-                $curl = curl_init("https://66f610a1436827ced975d41f.mockapi.io/abrigos/$idOuNome/pets");
-                curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-                curl_setopt($curl, CURLOPT_POST, true);
-                curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($pet));
-                curl_setopt($curl, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
-
-                $responseBody = curl_exec($curl);
-                $statusCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-
-                if ($statusCode == 201) {
-                    echo "Pet cadastrado com sucesso: $pet[nome]" . PHP_EOL;
-                } else if ($statusCode == 404) {
-                    echo "Id do abrigo não encontrado!" . PHP_EOL;
-                    break;
-                } else {
-                    echo "$statusCode - Erro ao cadastrar o pet: $pet[nome]" . PHP_EOL;
-                    echo $responseBody . PHP_EOL;
-                    break;
-                }
-            }
+            importarPets();
         } else if ($opcaoEscolhida == 5) {
             break;
         } else {
@@ -111,5 +42,89 @@ function listarAbrigos(): void
         $id = $abrigo['id'];
         $nome = $abrigo['nome'];
         echo "$id - $nome" . PHP_EOL;
+    }
+}
+
+function cadastrarAbrigo(): void
+{
+    echo "Digite o nome do abrigo:" . PHP_EOL;
+    $nome = trim(fgets(STDIN));
+    echo "Digite o telefone do abrigo:" . PHP_EOL;
+    $telefone = trim(fgets(STDIN));
+    echo "Digite o email do abrigo:" . PHP_EOL;
+    $email = trim(fgets(STDIN));
+
+    $abrigo = compact('nome', 'telefone', 'email');
+
+    $curl = curl_init('https://66f610a1436827ced975d41f.mockapi.io/abrigos');
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl, CURLOPT_POST, true);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($abrigo));
+    curl_setopt($curl, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+
+    $responseBody = curl_exec($curl);
+    $statusCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+    if ($statusCode < 400) {
+        echo "Abrigo cadastrado com sucesso!" . PHP_EOL;
+    } else {
+        echo "Erro ao cadastrar o abrigo:" . PHP_EOL;
+        echo $responseBody . PHP_EOL;
+    }
+}
+
+function listarPetsDeAbrigo(): void
+{
+    echo "Digite o id do abrigo:" . PHP_EOL;
+    $idOuNome = trim(fgets(STDIN));
+    $response = @file_get_contents("https://66f610a1436827ced975d41f.mockapi.io/abrigos/$idOuNome/pets");
+    if (!$response) {
+        echo "Id não cadastrado!" . PHP_EOL;
+        // continue;
+    }
+    $jsonArray = json_decode($response, true);
+    echo "Pets cadastrados:" . PHP_EOL;
+    foreach ($jsonArray as $pet) {
+        extract($pet);
+        echo "$id - $tipo - $nome - $raca - $idade ano(s)" . PHP_EOL;
+    }
+}
+
+function importarPets(): void
+{
+    echo "Digite o id do abrigo:" . PHP_EOL;
+    $idOuNome = trim(fgets(STDIN));
+
+    echo "Digite o nome do arquivo CSV:" . PHP_EOL;
+    $nomeArquivo = trim(fgets(STDIN));
+
+    $arquivo = fopen($nomeArquivo, 'r');
+    while ($campos = fgetcsv($arquivo)) {
+        $pet = [
+            'tipo' => $campos[0],
+            'nome' => $campos[1],
+            'raca' => $campos[2],
+            'idade' => $campos[3],
+            'cor' => $campos[4],
+            'peso' => $campos[5],
+        ];
+        $curl = curl_init("https://66f610a1436827ced975d41f.mockapi.io/abrigos/$idOuNome/pets");
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($pet));
+        curl_setopt($curl, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+
+        $responseBody = curl_exec($curl);
+        $statusCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+
+        if ($statusCode == 201) {
+            echo "Pet cadastrado com sucesso: $pet[nome]" . PHP_EOL;
+        } else if ($statusCode == 404) {
+            echo "Id do abrigo não encontrado!" . PHP_EOL;
+            break;
+        } else {
+            echo "$statusCode - Erro ao cadastrar o pet: $pet[nome]" . PHP_EOL;
+            echo $responseBody . PHP_EOL;
+            break;
+        }
     }
 }
